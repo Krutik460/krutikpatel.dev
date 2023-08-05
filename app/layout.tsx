e3fa3011178591e0432adabe6fd960ca2e7d1a2f
Krutik Patel <krutikpatel.patel@gmail.com>
2023-08-05 05:54:26 -0400

    adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import { Analytics } from "@vercel/analytics/react"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
@@ -42,7 +43,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="relative flex min-h-screen flex-col">
             <SiteHeader />
-            <div className="flex-1">{children}</div>
+            <div className="flex-1">
+              {children}
+              <Analytics />
+            </div>
           </div>
           <TailwindIndicator />
         </ThemeProvider>
