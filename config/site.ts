c07cf159ebf3ab00f82baf187f3a48b07a9c7a00
Krutik Patel <krutikpatel.patel@gmail.com>
2023-08-22 18:11:30 -0400

    adding navigation options

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,16 @@ export const siteConfig = {
   name: "Krutik Patel",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
-  mainNav: [],
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Blog",
+      href: "/blog",
+    },
+  ],
   links: {
     twitter: "https://twitter.com/krutik460",
     github: "https://github.com/Krutik460",
