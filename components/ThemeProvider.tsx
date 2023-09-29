82370812d091e4f21548b89bac7cc3c995da8622
Krutik Patel <krutikpatel.patel@gmail.com>
2023-09-29 07:13:59 -0400

    pushing bug fix regarding module import error

---
+++ components/ThemeProvider.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+"use client"
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProviderProps } from "next-themes/dist/types"
+
+export default function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
