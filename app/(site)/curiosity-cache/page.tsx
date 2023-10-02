7ae53ec42ae12322a2601ad3ceca6a9142a480a5
Krutik Patel <krutikpatel.patel@gmail.com>
2023-10-01 20:07:04 -0400

    adding curiosity-cache page

---
+++ app/(site)/curiosity-cache/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { draftMode } from "next/headers"
+
+import { token } from "@/sanity/lib/sanityFetch"
+import PreviewProvider from "@/components/PreviewProvider"
+
+export const metadata = {
+  title: "Blogs",
+  description: "This section includes blogs written by Krutik Patel.",
+}
+
+export default async function BlogPage() {
+  const isDraftMode = draftMode().isEnabled
+
+  if (isDraftMode && token) {
+    return (
+      <PreviewProvider token={token}>
+        <h1>Hello</h1>
+      </PreviewProvider>
+    )
+  }
+
+  return <h1>Real</h1>
+}
