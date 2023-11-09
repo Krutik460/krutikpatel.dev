501e3e7491a46b5c9dc21927999ef3a29354b8a1
Krutik Patel <krutikpatel.patel@gmail.com>
2023-11-09 18:52:59 -0500

    removed white space and back arrow on smaller screen

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,6 @@
 export default function IndexPage() {
   return (
-    <section className="flex min-h-screen max-w-full items-center justify-center">
+    <section className="flex min-h-[calc(100vh-4rem)] max-w-full items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-2">
         <h1 className="max-w-[980px] text-center	text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Krutik Patel
--- components/blog/Blog.tsx
+++ components/blog/Blog.tsx
@@ -17,15 +17,16 @@ export function Blog({
 }) {
   return (
     <div>
-      <div className="container mx-auto flex max-w-5xl flex-row py-6 lg:py-10">
-        <div className="basis-1/4 justify-center py-6 lg:py-10">
-          <Link
-            href="/blog"
-            className={cn(buttonVariants({ variant: "link" }))}
-          >
-            <Icons.chevronLeft className="mr-2 h-5 w-5 text-muted-foreground hover:text-accent-foreground md:h-8 md:w-8" />
-          </Link>
-        </div>
+      <div className="container mx-auto flex max-w-5xl flex-row pb-0 pt-6 lg:py-10">
+        <Link
+          href="/blog"
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "hidden basis-1/4 justify-center py-2 md:inline-flex lg:py-10"
+          )}
+        >
+          <Icons.chevronLeft className="mr-2 h-5 w-5 text-muted-foreground hover:text-accent-foreground md:h-8 md:w-8" />
+        </Link>
         <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
           <DocsPageHeader
             heading={blogInfo.title}
@@ -33,42 +34,44 @@ export function Blog({
           />
         </div>
       </div>
-      <hr className="my-8" />
-      {posts?.length ? (
-        <div className="grid gap-10 sm:grid-cols-2">
-          {posts.map((post, index) => (
-            <article
-              key={index}
-              className="group relative flex flex-col space-y-2"
-            >
-              {post.mainImage && (
-                <Image
-                  src={urlForImage(post.mainImage).url()}
-                  alt=""
-                  width={804}
-                  height={452}
-                  className="rounded-md border bg-muted transition-colors"
-                  priority={index <= 1}
-                />
-              )}
-              <h2 className="text-xl font-bold">{post.title}</h2>
-              {post.publishedAt && (
-                <p className="text-sm text-muted-foreground">
-                  {formatDate(post.publishedAt)}
-                </p>
-              )}
-              <Link
-                href={`blog/${blogInfo.slug.current}/${post.slug.current}`}
-                className="absolute inset-0"
+      <hr className="m-0 mb-2 hidden md:my-8 md:block" />
+      <div className="mb-14">
+        {posts?.length ? (
+          <div className="grid gap-10 sm:grid-cols-2">
+            {posts.map((post, index) => (
+              <article
+                key={index}
+                className="group relative flex flex-col space-y-2"
               >
-                <span className="sr-only">View Article</span>
-              </Link>
-            </article>
-          ))}
-        </div>
-      ) : (
-        <p>No posts published.</p>
-      )}
+                {post.mainImage && (
+                  <Image
+                    src={urlForImage(post.mainImage).url()}
+                    alt=""
+                    width={804}
+                    height={452}
+                    className="rounded-md border bg-muted transition-colors"
+                    priority={index <= 1}
+                  />
+                )}
+                <h2 className="text-xl font-bold">{post.title}</h2>
+                {post.publishedAt && (
+                  <p className="text-sm text-muted-foreground">
+                    {formatDate(post.publishedAt)}
+                  </p>
+                )}
+                <Link
+                  href={`blog/${blogInfo.slug.current}/${post.slug.current}`}
+                  className="absolute inset-0"
+                >
+                  <span className="sr-only">View Article</span>
+                </Link>
+              </article>
+            ))}
+          </div>
+        ) : (
+          <p>No posts published.</p>
+        )}
+      </div>
     </div>
   )
 }
