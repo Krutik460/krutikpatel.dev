b42d7c2d1f7f70e25c6c391233ce7f0020e75658
Krutik Patel <krutikpatel.patel@gmail.com>
2023-11-21 21:18:53 -0500

    fix umami src url

--- components/UmamiAnalytics.tsx
+++ components/UmamiAnalytics.tsx
@@ -7,7 +7,7 @@ export default function UmamiAnalytics() {
     <>
       <Script
         async
-        src="https://umami-krutik-rigjcof4u-krutik460.vercel.app/script.js"
+        src="https://umami-krutik-dev.vercel.app/script.js"
         data-website-id="bb497e0c-7e0e-4e53-a542-35bf60053da4"
       />
     </>
