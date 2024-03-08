80aafc84da1af10d0447ccdcdfb33e320bbf0e59
Krutik Patel <krutikpatel.patel@gmail.com>
2024-03-08 02:10:27 -0500

    update umami website id

--- components/UmamiAnalytics.tsx
+++ components/UmamiAnalytics.tsx
@@ -2,13 +2,16 @@
 
 import Script from "next/script"
 
+const UMAMI_WEBSITE_ID = process.env.UMAMI_WEBSITE_ID
+
 export default function UmamiAnalytics() {
+  console.log(process.env.UMAMI_WEBSITE_ID)
   return (
     <>
       <Script
         async
         src="https://umami-krutik-dev.vercel.app/script.js"
-        data-website-id="bb497e0c-7e0e-4e53-a542-35bf60053da4"
+        data-website-id={process.env.UMAMI_WEBSITE_ID}
       />
     </>
   )
