3db301dd52ac69319c6fa8a22ac4a85ac7a018b2
Krutik Patel <krutikpatel.patel@gmail.com>
2024-03-15 15:49:04 -0400

    adding umami website id

--- components/UmamiAnalytics.tsx
+++ components/UmamiAnalytics.tsx
@@ -11,7 +11,7 @@ export default function UmamiAnalytics() {
       <Script
         async
         src="https://www.analytics.krutikpatel.dev/script.js"
-        data-website-id={process.env.UMAMI_WEBSITE_ID}
+        data-website-id="0b3aba2e-1c7f-4f9b-93e8-e891243c5033"
       />
     </>
   )
