ca0d5367784fc1737f4fad2bc5fc144c4772df65
Krutik Patel <krutikpatel.patel@gmail.com>
2024-05-08 17:26:42 -0400

    adding loading for posts

--- components/blog/Blog.tsx
+++ components/blog/Blog.tsx
@@ -7,6 +7,8 @@ import { cn, formatDate } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/Button"
 import { Icons } from "@/components/Icons"
 import { Separator } from "@/components/ui/separator"
+import { Suspense } from "react"
+import PostLoading from "@/components/loading/PostLoading"
 
 export function Blog({
   blogInfo,
@@ -43,37 +45,39 @@ export function Blog({
       <Separator className="m-0 mb-2 hidden md:mt-8 md:block" />
       <div className="mb-14 mt-4 md:mt-8">
         {posts?.length ? (
-          <div className="grid gap-10 sm:grid-cols-2">
-            {posts.map((post, index) => (
-              <article
-                key={index}
-                className="group relative flex flex-col space-y-2"
-              >
-                {post.mainImage && (
-                  <Image
-                    src={urlForImage(post.mainImage).url()}
-                    alt=""
-                    width={804}
-                    height={452}
-                    className="rounded-md border bg-muted transition-colors"
-                    priority={index <= 1}
-                  />
-                )}
-                <h2 className="text-xl font-bold">{post.title}</h2>
-                {post.publishedAt && (
-                  <p className="text-sm text-muted-foreground">
-                    {formatDate(post.publishedAt)}
-                  </p>
-                )}
-                <Link
-                  href={`blog/${blogInfo.slug.current}/${post.slug.current}`}
-                  className="absolute inset-0"
+          <Suspense fallback={<PostLoading />}>
+            <div className="grid gap-10 sm:grid-cols-2">
+              {posts.map((post, index) => (
+                <article
+                  key={index}
+                  className="group relative flex flex-col space-y-2"
                 >
-                  <span className="sr-only">View Article</span>
-                </Link>
-              </article>
-            ))}
-          </div>
+                  {post.mainImage && (
+                    <Image
+                      src={urlForImage(post.mainImage).url()}
+                      alt=""
+                      width={804}
+                      height={452}
+                      className="rounded-md border bg-muted transition-colors"
+                      priority={index <= 1}
+                    />
+                  )}
+                  <h2 className="text-xl font-bold">{post.title}</h2>
+                  {post.publishedAt && (
+                    <p className="text-sm text-muted-foreground">
+                      {formatDate(post.publishedAt)}
+                    </p>
+                  )}
+                  <Link
+                    href={`blog/${blogInfo.slug.current}/${post.slug.current}`}
+                    className="absolute inset-0"
+                  >
+                    <span className="sr-only">View Article</span>
+                  </Link>
+                </article>
+              ))}
+            </div>
+          </Suspense>
         ) : (
           <p>No posts published.</p>
         )}
---
+++ components/loading/PostLoading.tsx
@@ -0,0 +1,11 @@
+export default async function PostLoading() {
+  return (
+    <div className="mx-auto w-full max-w-sm rounded-md border border-blue-300 p-4 shadow">
+      <article className="group relative flex animate-pulse flex-col space-y-2">
+        <div className="h-[452px] w-[804px] rounded-full bg-slate-700"></div>
+        <div className="h-7 rounded bg-slate-700"></div>
+        <div className="h-5 rounded bg-slate-700"></div>
+      </article>
+    </div>
+  )
+}
